54340b2b4958711c3563f93adc939d5ac63bb9af
asdf1899 <araid.anas99@gmail.com>
2018-11-22 23:27:06 +0100

    Add libraries

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+  <head>
+    <?php
+      include "core/_header.php";
+     ?>
+  </head>
+  <body>
+    <div class="mdl-layout mdl-js-layout">
+      <header class="mdl-layout__header mdl-layout__header--transparent">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">Curie-differenziata</span>
+          <div class="mdl-layout-spacer"></div>
+          <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+          </nav>
+        </div>
+      </header>
+      <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Curie-differenziata</span>
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+      </div>
+      <main class="mdl-layout__content">
+        <div class="mdl-grid">
+          <div class="mdl-cell mdl-cell--8-col mdl-cell--6-col-tablet">6 (8 tablet)</div>
+          <div class="mdl-cell mdl-cell--4-col mdl-cell--2-col-tablet">4 (6 tablet)</div>
+        </div
+      </main>
+    </div>
+  </body>
+</html>
