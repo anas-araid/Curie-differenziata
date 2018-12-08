a30e6d2b209398d150e8ce9a18332064d81dfc9d
asdf1899 <araid.anas99@gmail.com>
2018-12-08 23:30:10 +0100

    Completed homepage

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
       include "core/_header.php";
      ?>
   </head>
-  <body>
+  <body style="margin:0">
     <div class="mdl-layout mdl-js-layout">
       <header class="mdl-layout__header mdl-layout__header--transparent">
         <div class="mdl-layout__header-row">
@@ -17,24 +17,35 @@
         </div>
       </header>
       <div class="mdl-layout__drawer">
-        <span class="mdl-layout-title style-text-color" style="font-weight:500">Curie</span><span class="mdl-layout-title" style="color:grey;font-weight:100">Differenziata</span>
+        <span class="mdl-layout-title style-text-green" style="font-weight:500">Curie</span><span class="mdl-layout-title style-text-grey" style="font-weight:100">Differenziata</span>
         <nav class="mdl-navigation">
           <a class="mdl-navigation__link" href="">Login</a>
           <a class="mdl-navigation__link" href="">Progetto</a>
         </nav>
       </div>
-      <main class="mdl-layout__content">
+      <main class="mdl-layout__content" style="margin:0">
         <section class="mdl-cell--hide-desktop" style="width:100%;margin:35px">
           <h4 class="style-text-green" style="font-weight:500;display:inline">Curie</h4><h4 class="style-text-grey" style="font-weight:100;display:inline">Differenziata</h4>
         </section>
         <section>
-          <div class="mdl-grid style-parent">
-            <div class="mdl-cell mdl-cell--8-col mdl-cell--6-col-tablet style-child">
+          <div class="mdl-grid">
+            <div class="mdl-cell mdl-cell--7-col mdl-cell--6-col-tablet">
               <h3 class="style-text-grey">Valuta la raccolta differenziata in modo <span class="style-text-green">facile</span> e <span class="style-text-green">veloce...</span></h3>
-              <button>INIZIA SUBUTO!</button>
+              <div style="text-align:center">
+                <br>
+                <button class="special-button">INIZIA SUBUTO!</button>
+              </div>
             </div>
-            <div class="mdl-cell mdl-cell--4-col mdl-cell--2-col-tablet style-child" style="padding:20px;margin:20px">
-              <img src="img/recycling.png"></img>
+            <div class="mdl-cell mdl-cell--5-col mdl-cell--2-col-tablet mdl-cell--hide-phone">
+              <img src="img/recycling.png" style="width:50%"></img>
+            </div>
+          </div>
+          <div class="mdl-grid" style="background:url(img/bg.svg);background-repeat:no-repeat;background-size:cover;margin:0;bottom:0">
+            <div class="mdl-cell mdl-cell--12-col mdl-cell--8-col-tablet">
+              <img src="img/bg1.png" style="width:100%"></img>
+              <div>
+                <p style="color:white;font-size:1.6vw">Copyright 2018 Anas Araid in collaborazione con Marie Curie Pergine</p>
+              </div>
             </div>
           </div>
         </section>
