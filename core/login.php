2bfa06df51cbef297268db74a6a759e9ddc0dfbf
asdf1899 <araid.anas99@gmail.com>
2018-12-14 08:20:27 +0100

    Init login and getData

--- core/login.php
+++ core/login.php
@@ -1,6 +1,10 @@
 <form action="" method="POST" style="text-align:center">
   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-    <input class="mdl-textfield__input" type="number" id="codice" name="codice" required="">
+    <select class="mdl-textfield__input" type="select" id="codice" name="codice" required="">
+      <?php
+
+       ?>
+    </select>
     <label class="mdl-textfield__label" for="codice">Codice</label>
   </div>
   <br>
@@ -15,3 +19,33 @@
     <button class="special-button" type="reset" onclick="location.href='core/logout.php'">Indietro</button>
   </div>
 </form>
+<?php
+  if(isset($_POST['password'])){
+    include 'core/functions.php';
+    $codice = text_filter_encrypt($_POST["codice"]);
+    $password = text_filter_encrypt($_POST["password"]);
+    $selectQuery = "SELECT * FROM t_operatori WHERE Codice='$codice' AND Password='$password'";
+    $select = mysqli_query($db_conn, $selectQuery);
+    if ($select==null){
+      die('error');
+        //throw new exception ("Impossibile aggiornare l'utente");
+    }
+    $esistenzaUtente = false;
+    while($ris = mysqli_fetch_array ($select, MYSQLI_ASSOC)){
+      $esistenzaUtente = true;
+      $_SESSION['include'] = ' ';
+      echo "
+      <script>
+      flatAlert('Accesso eseguito con successo', '', 'success', 'core/log.php');
+      </script>";
+    }
+    if (!$esistenzaUtente){
+      echo "
+      <script>
+      flatAlert('Password errata', '', 'error', 'index.php');
+      </script>";
+    }
+
+  }
+
+ ?>
